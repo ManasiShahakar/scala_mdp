1d3b2ef92141d62f369473996b9b55b32e33ccbe
Manasi Shahakar <manasi-sunil.shahakar@capgemini.com>
2022-02-13 22:38:17 +0530

    arithmatic

--- src/main/scala/Scala_concepts/Arithmatic.scala
+++ src/main/scala/Scala_concepts/Arithmatic.scala
@@ -1,5 +1,22 @@
 package Scala_concepts
-
+import scala.io.StdIn._
 object Arithmatic {
+    def main(args: Array[String]) {
+      println("Arithmetic operations ")
+      println("Enter the first number:")
+      var m1: Int = readInt()
+      println("Enter the second number:")
+      var m2: Int = readInt()
+      println("Addition= " + (m1+m2))
+      println("Subtraction= " + (m2-m1))
+      println("Multiplication= " + (m1*m2))
+      println("Division= " + (m2/m1))
+      if ((m1*m2) % 2 ==0){
+        println("The addition of two numbers is even")
+      }
+      else {
+        println("The addition of two numbers is odd")
+      }
+    }
 
 }
